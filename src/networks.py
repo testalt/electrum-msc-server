79a9cc9cf996b9b32a3b99751dce9563d9a517de
testalt <jmcafee321@gmail.com>
2014-11-10 16:36:41 +0800

    first commit
    first commit

--- src/networks.py
+++ src/networks.py
@@ -11,7 +11,7 @@ params = {
         'script_address': 196,
         'genesis_hash': '000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943'
     },
-    'masccoin_main': {
+    'msccoin_main': {
         'pubkey_address': 0,
         'script_address': 5,
         'genesis_hash': '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
